f71468d48d9b5d7cac0721f8e46476a5bd00daf5
Sergey <sergstas0@gmail.com>
2021-04-08 18:50:56 +0500

    client form & validation

--- neurotestclient/src/app/app.module.ts
+++ neurotestclient/src/app/app.module.ts
@@ -3,18 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ClientFormComponent } from './client-form/client-form.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { TestingModuleComponent } from './testing-module/testing-module.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'client-form', component: ClientFormComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientFormComponent,
+    TestingModuleComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [
     RouterModule
---
+++ neurotestclient/src/app/shared/client.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from "@angular/core";
+
+export class Client {
+  lastName: string;
+  firstName: string;
+  secondName?: string;
+  sex: string;
+  birthDate: string;
+  address: string;
+  job: string;
+  diseases: string;
+  phoneNumber: string;
+}
+
+@Injectable({providedIn: "root"})
+export class ClientService {
+
+}
